7978b8359cc4ff53d520c45a827b6e9b76260ad4
Jordi Garcia <jordigg@users.noreply.github.com>
2016-06-02 17:35:46 +0100

    Enable ensure false (#2)
    enable ensure false

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,40 @@
-define windows_xmltask($taskname = $title, $xmlfile, $overwrite = 'false', $ensure = 'present') {
+define windows_xmltask($taskname = $title, $xmlfile, $overwrite = false, $ensure = 'present') {
   if ! ($ensure in [ 'present', 'absent' ]) {
     fail('valid values for ensure are \'present\' or \'absent\'')
   }
-  $null = '$null'
-  $temp_filename = fqdn_rand(3000, "$taskname")
+  $null  = '$null'
+  $false = '$false'
+  $temp_filename = fqdn_rand(3000, $taskname)
   if ($ensure == 'present') {
-    if ($overwrite == 'true'){
+    if ($overwrite == true){
       $is_force = '-Force'
     }
     file {"c:\\Users\\Public\\$temp_filename.xml":
-      ensure  => file,
+      ensure             => file,
       source_permissions => 'ignore',
-      source  => $xmlfile
+      source             => $xmlfile,
     } ->
     exec { "Importing task $taskname":
-      command => "
+      command  => "
         Try{
           if((Get-ScheduledTask '$taskname') -eq $null){
             Register-ScheduledTask -Xml (get-content 'C:\Users\Public\\$temp_filename.xml' | out-string) -TaskName '$taskname' $is_force
           }
           Remove-Item 'c:\Users\Public\\$temp_filename.xml'
+        }
+        Catch{
+          exit 0
+        }
+      ",
+      provider  => powershell,
+    }
+  }else{
+    exec { "Removing task $taskname":
+      command => "
+        Try{
+          if((Get-ScheduledTask '$taskname') -ne $null){
+            Unregister-ScheduledTask -TaskName '$taskname' -Confirm:$false
+          }
         }
         Catch{
           exit 0
