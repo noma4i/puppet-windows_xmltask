14a6c5ccaca3ba08fcd4932185d8d7cfce30ec47
Alexander Tsirel <noma4i@gmail.com>
2016-06-04 13:44:15 +1000

    lint to have better score at Puppet Forge

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,38 +9,38 @@ define windows_xmltask($taskname = $title, $xmlfile, $overwrite = false, $ensure
     if ($overwrite == true){
       $is_force = '-Force'
     }
-    file {"c:\\Users\\Public\\$temp_filename.xml":
+    file {"c:\\Users\\Public\\${temp_filename}.xml":
       ensure             => file,
       source_permissions => 'ignore',
       source             => $xmlfile,
     } ->
-    exec { "Importing task $taskname":
+    exec { "Importing task ${taskname}":
       command  => "
         Try{
-          if((Get-ScheduledTask '$taskname') -eq $null){
-            Register-ScheduledTask -Xml (get-content 'C:\Users\Public\\$temp_filename.xml' | out-string) -TaskName '$taskname' $is_force
+          if((Get-ScheduledTask '${taskname}') -eq ${null}){
+            Register-ScheduledTask -Xml (get-content 'C:\Users\Public\\${temp_filename}.xml' | out-string) -TaskName '${taskname}' ${is_force}
           }
-          Remove-Item 'c:\Users\Public\\$temp_filename.xml'
+          Remove-Item 'c:\Users\Public\\${temp_filename}.xml'
         }
         Catch{
           exit 0
         }
       ",
-      provider  => powershell,
+      provider => powershell,
     }
   }else{
-    exec { "Removing task $taskname":
-      command => "
+    exec { "Removing task ${taskname}":
+      command  => "
         Try{
-          if((Get-ScheduledTask '$taskname') -ne $null){
-            Unregister-ScheduledTask -TaskName '$taskname' -Confirm:$false
+          if((Get-ScheduledTask '${taskname}') -ne ${null}){
+            Unregister-ScheduledTask -TaskName '${taskname}' -Confirm:${false}
           }
         }
         Catch{
           exit 0
         }
       ",
-      provider  => powershell,
+      provider => powershell,
     }
   }
 }
